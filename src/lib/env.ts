da15911e797a0e250bab9e1a427a8e886c328c72
Rohit Saini <rohitbhatwara1@gmail.com>
2025-07-30 21:38:34 +0200

    feat(auth): setup clerk for user authentication

---
+++ src/lib/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    CLERK_SECRET_KEY: z.string(),
+  },
+
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  },
+
+  emptyStringAsUndefined: true,
+
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+})
